1b3750f389f67b51e9834eccbc4a17cac4133f3f
kanishkk19 <kannu1904@gmail.com>
2023-01-26 11:51:09 +0530

    new

--- script.js
+++ script.js
@@ -16,32 +16,32 @@ let songs = [
     coverPath: "covers/1.jpg",
   },
   {
-    songName: "Cielo - Huma-Huma",
+    songName: "Lil Uzi Vert - The Way Life Goes",
     filePath: "songs/2.mp3",
     coverPath: "covers/2.jpg",
   },
   {
-    songName: "DEAF KEV - Invincible [NCS Release]-320k",
+    songName: "Baby Keem, Kendrick Lamar - family ties",
     filePath: "songs/3.mp3",
     coverPath: "covers/3.jpg",
   },
   {
-    songName: "Different Heaven & EH!DE - My Heart [NCS Release]",
+    songName: "SZA - Open Arms ft. Travis Scott",
     filePath: "songs/4.mp3",
     coverPath: "covers/4.jpg",
   },
   {
-    songName: "Janji-Heroes-Tonight-feat-Johnning-NCS-Release",
+    songName: "Kanye West - Heartless",
     filePath: "songs/5.mp3",
     coverPath: "covers/5.jpg",
   },
   {
-    songName: "Rabba - Salam-e-Ishq",
+    songName: "J.Cole - No Role Modelz",
     filePath: "songs/2.mp3",
     coverPath: "covers/6.jpg",
   },
   {
-    songName: "Sakhiyaan - Salam-e-Ishq",
+    songName: "French Montana - Unforgettable ft. Swae Lee",
     filePath: "songs/2.mp3",
     coverPath: "covers/7.jpg",
   },
